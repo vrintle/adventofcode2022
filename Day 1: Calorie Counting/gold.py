195526e5b5759b87012b456010483115b1349ed4
Rahul Verma <33773797+vrintle@users.noreply.github.com>
2022-12-02 10:52:43 +0530

    Create gold.py

---
+++ Day 1: Calorie Counting/gold.py
@@ -0,0 +1,16 @@
+a = []
+sum = 0
+while True:
+	try:
+		s = input()
+	except:
+		break
+	if len(s) == 0:
+		a.append(sum)
+		sum = 0
+	else:
+		sum = sum + int(s)
+a.append(sum)
+a.sort()
+# print(a)
+print(a[-1] + a[-2] + a[-3])
